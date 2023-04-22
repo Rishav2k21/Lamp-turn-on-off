463cffd6b9c6b930cfb90012bb7af44ecf9fc151
Rishav Anand <rishavanand2k21@gmail.com>
2023-04-22 18:25:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var btn=document.getElementById("btn");
+var light=document.getElementById("light");
+
+function togglebtn(){
+    btn.classList.toggle("active");//ye jaise hi click hoga ye active wala css property bhi ghus jyega uss btn me
+    light.classList.toggle("on");//ye jaise hi click hoga ye active wala css property bhi ghus jyega uss btn me
+
+    
+}
